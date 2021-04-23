4d5a2175a6367396bf5025be22649f64920cb35b
hanwenzhang123 <hanwenzhang888@gmail.com>
2021-04-22 23:50:25 -0400

    basic setup

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import (Flask, render_template, 
+                    url_for, request)
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/add-pet', methods=['GET', 'POST'])
+def add_pet():
+    if request.form:
+        print(request.form)
+        print(request.form['name'])
+    return render_template('addpet.html')
+
+@app.route('/pet')
+def pet():
+    return render_template('pet.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000, host='127.0.0.1')
